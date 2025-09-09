4f0e27c9e80aa8f02d6425189d740ad18837cebd
Ajayrama Kumaraswamy <ajkumaraswamy@tutamail.com>
2025-09-09 11:17:24 +0200

    feat: made installation configurations compatible with PEP517

--- setup.py
+++ setup.py
@@ -1,30 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-install_requires = [
-                       'scipy',
-                       'pyqtgraph',
-                       'pandas',
-                       'matplotlib',
-                       'tifffile',
-                       'scikit-image'
-                   ],
+setup()
 
-try:
-    import PyQt5
-except ModuleNotFoundError as mnfe:
-    install_requires += ["PyQt5>=5.12.2,<5.13"]
-
-config = {
-    'description': 'ILTIS - an interactive labeled-trial image stack slicer',
-    'use_scm_version': True,
-    'setup_requires': ['setuptools_scm'],
-    'install_requires': install_requires,
-    'python_requires': ">=3.7",
-    "packages": find_packages(),
-    "package_data": {"iltis": ["graphics/icons/*.svg", "graphics/*.jpg"]},
-    'scripts': [],
-    "entry_points": {"console_scripts": ["iltis = iltis.Main:main"]},
-    'name': 'iltis'
-}
-
-setup(**config)
